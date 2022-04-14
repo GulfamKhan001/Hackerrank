36f6e1c9d8edc3159ef6d0c46d7ae80974302ae4
GulfamKhan001 <gulfamkajla@gmail.com>
2022-04-14 12:30:21 +0530

    :zap: Login Page

--- hackerrank.js
+++ hackerrank.js
@@ -4,9 +4,21 @@ let browserOpenPromise = puppeteer.launch({
     headless: false,
     defaultViewport : null,
     args: ["--start-maximized"],
-    executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+    //  executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 });
 
-browserOpenPromise.then(function print(){
+browserOpenPromise.then(function (browser){
     console.log("Browser Opened");
-});
+    let allTabs=browser.pages();
+    return allTabs;
+})
+.then(function(allTabsArr){
+    cTab=allTabsArr[0];
+    console.log("new Tab");
+
+    let visitToSite= cTab.goto("https://www.hackerrank.com/auth/login");
+    return visitToSite;
+})
+.then(function(data){
+    console.log("Login Page opened");
+})
