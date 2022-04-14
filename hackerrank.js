19a793b4f50014c7ad4840a2b4995bf2050bb5c9
GulfamKhan001 <gulfamkajla@gmail.com>
2022-04-14 18:21:50 +0530

    :fire: Logined Succesfully

--- hackerrank.js
+++ hackerrank.js
@@ -21,7 +21,7 @@ browserOpenPromise.then(function (browser){
     let visitToSite= cTab.goto("https://www.hackerrank.com/auth/login");
     return visitToSite;
 })
-.then(function(data){
+.then(function(){
     console.log("Login Page opened");
 
     let emailFilled=cTab.type("#input-1",email);
@@ -35,7 +35,35 @@ browserOpenPromise.then(function (browser){
 })
 .then(function(){
     console.log("Password Typed");
+
+    let loginPage=cTab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+    return loginPage; 
+})
+.then(function(){
+    console.log("Page Logined");
+
+    let completePage=waitAndClick(".nav-link.contests");// Wait for Page to load fully and then click
+    // { delay: 10000 }
+    return completePage
+})
+.then(function(){
+    console.log("Complete Page Reached");
 })
 .catch(function(err){
     console.log(err);
-})
+});
+
+
+function waitAndClick(selector){
+    let myPromise = new Promise(function (resolve, reject) {
+        let waitForbutton=cTab.waitForSelector(selector);
+        waitForbutton.then(function (){
+            let clickpromise= cTab.click(selector);
+            return clickpromise;
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+    });
+    return myPromise;
+}
