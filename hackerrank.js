d79f9022881b657849782a8fe41ac9494b3b623a
GulfamKhan001 <gulfamkajla@gmail.com>
2022-04-16 10:53:37 +0530

    :zap: Completed

--- hackerrank.js
+++ hackerrank.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
 let { email, password } = require('./login');
-
+let {answer} = require('./codes')
 
 let browserOpenPromise = puppeteer.launch({
     headless: false,
@@ -68,10 +68,28 @@ browserOpenPromise.then(function (browser){
 })
 .then(function (linksArr) {
     console.log("links to all ques received");
+    //question solve krna h
+    //link to the question to besolved, idx of the linksArr
+    let questionWillBeSolvedPromise = questionSolver(linksArr[0], 0);
+    for (let i = 1; i < linksArr.length; i++){
+    questionWillBeSolvedPromise = questionWillBeSolvedPromise.then(function () {
+    return questionSolver(linksArr[i], i);
+    })
+    // a = 10;
+    // a = a + 1;
+    }
+    return questionWillBeSolvedPromise;
+})
+.then(function () {
+    console.log("question is solved");
 })
 .catch(function(err){
     console.log(err);
 });
+
+
+
+
 
 
 function waitAndClick(selector){
@@ -86,9 +104,85 @@ function waitAndClick(selector){
             resolve();
         })
         .catch(function(err){
-            console.log(err);
-            reject();
+            reject(err);
         });
     });
     return myPromise;
 }
+
+function questionSolver(url, idx) {
+    return new Promise(function (resolve, reject) {
+      let fullLink = `https://www.hackerrank.com${url}`;
+      let goToQuesPagePromise = cTab.goto(fullLink);
+      goToQuesPagePromise
+        .then(function () {
+          console.log("question opened");
+          //tick the custom input box mark
+          let waitForCheckBoxAndClickPromise = waitAndClick(".checkbox-input");
+          return waitForCheckBoxAndClickPromise;
+        })
+        .then(function () {
+          //select the box where code will be typed
+          let waitForTextBoxPromise = cTab.waitForSelector(".custominput");
+          return waitForTextBoxPromise;
+        })
+        .then(function () {
+          let codeWillBeTypedPromise = cTab.type(".custominput", answer[idx], {
+            delay: 100,
+          });
+          return codeWillBeTypedPromise;
+        })
+        .then(function () {
+          //control key is pressed promise
+          let controlPressedPromise = cTab.keyboard.down("Control");
+          return controlPressedPromise;
+        })
+        .then(function () {
+          let aKeyPressedPromise = cTab.keyboard.press("a");
+          return aKeyPressedPromise;
+        })
+        .then(function () {
+          let xKeyPressedPromise = cTab.keyboard.press("x");
+          return xKeyPressedPromise;
+        })
+        .then(function () {
+          let ctrlIsReleasedPromise = cTab.keyboard.up("Control");
+          return ctrlIsReleasedPromise;
+        })
+        .then(function () {
+          //select the editor promise
+          let cursorOnEditorPromise = cTab.click(
+            ".monaco-editor.no-user-select.vs"
+          );
+          return cursorOnEditorPromise;
+        })
+        .then(function () {
+          //control key is pressed promise
+          let controlPressedPromise = cTab.keyboard.down("Control");
+          return controlPressedPromise;
+        })
+        .then(function () {
+          let aKeyPressedPromise = cTab.keyboard.press("A");
+          return aKeyPressedPromise;
+        })
+        .then(function () {
+          let vKeyPressedPromise = cTab.keyboard.press("V");
+          return vKeyPressedPromise;
+        })
+        .then(function () {
+          let controlDownPromise = cTab.keyboard.up("Control");
+          return controlDownPromise;
+        })
+        .then(function () {
+          let submitButtonClickedPromise = cTab.click(".hr-monaco-submit");
+          return submitButtonClickedPromise;
+        })
+        .then(function () {
+          console.log("code submitted successfully");
+          resolve();
+        })
+        .catch(function (err) {
+          reject(err);
+        });
+    });
+  }
